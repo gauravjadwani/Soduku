80cfc1f347257dd812bd852b78931d03376704eb
Cosmonaut <gaurav.jadwani@hotmail.com>
2019-06-26 08:58:56 +0530

    Dev (#3)
    * Initial commit from Create React App
    
    * init
    
    * table
    
    * ui done
    
    * insertion happend
    
    * UI completed
    
    * functions added
    
    * functions added
    
    * Sanitization (#2)
    
    * logic improved
    
    * values placed
    
    * values placed
    
    * random values inserted
    
    * logic part done
    
    * sanitization completed
    
    * completed
    
    * completed
    
    * completed
    
    * final

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import CustomTable from './components/Table';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <CustomTable />
+    </div>
+  );
+};
+
+export default App;
