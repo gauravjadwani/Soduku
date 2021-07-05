ebfea5a5161d1226d1f98e93d5d9cbc618833987
Gaurav <gaurav.jadwani@hotmail.com>
2021-07-05 16:06:37 +0530

    Updates

---
+++ precache-manifest.b79326852d199d54326e746d389a0bac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25db29ddee00961518b99d4610a9391f",
+    "url": "/Soduko/index.html"
+  },
+  {
+    "revision": "26dddb8e024958706a36",
+    "url": "/Soduko/static/css/main.0abdea0e.chunk.css"
+  },
+  {
+    "revision": "2249d86949b54dc0e89e",
+    "url": "/Soduko/static/js/2.09aed800.chunk.js"
+  },
+  {
+    "revision": "26dddb8e024958706a36",
+    "url": "/Soduko/static/js/main.14fd9a15.chunk.js"
+  },
+  {
+    "revision": "4dd7e776265f7982e6b0",
+    "url": "/Soduko/static/js/runtime~main.4fb96c79.js"
+  }
+]);
