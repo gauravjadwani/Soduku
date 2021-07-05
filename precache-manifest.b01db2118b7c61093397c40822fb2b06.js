5aa30db1412988bf779560863cf06a9b51e29832
Gaurav <gaurav.jadwani@hotmail.com>
2021-07-05 19:07:18 +0530

    Updates

---
+++ precache-manifest.b01db2118b7c61093397c40822fb2b06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063a5e917cdb974429379439f496fdf8",
+    "url": "/Soduku/index.html"
+  },
+  {
+    "revision": "b74a6a3c0519d6171d80",
+    "url": "/Soduku/static/css/main.0abdea0e.chunk.css"
+  },
+  {
+    "revision": "2249d86949b54dc0e89e",
+    "url": "/Soduku/static/js/2.09aed800.chunk.js"
+  },
+  {
+    "revision": "b74a6a3c0519d6171d80",
+    "url": "/Soduku/static/js/main.86ea40f6.chunk.js"
+  },
+  {
+    "revision": "83a141ba2512176138bf",
+    "url": "/Soduku/static/js/runtime~main.112034a4.js"
+  }
+]);
